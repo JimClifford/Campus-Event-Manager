2a591f0f2993adfd9bab231f48adfe34001d26fe
naomikonlack <96690082+naomikonlack@users.noreply.github.com>
2024-02-22 00:23:12 +0000

    login_view.php

--- login/login_view.php
+++ login/login_view.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../function/login_controller.php'; //Require the login controller
 ?> 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,39 +8,32 @@ require_once '../function/login_controller.php'; //Require the login controller
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login | Campus Event Scheduler</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <style>
-    .login-container {
-      max-width: 400px;
-      margin: auto;
-      margin-top: 100px;
-      padding: 20px;
-      border: 1px solid #ccc;
-      border-radius: 10px;
-    }
-    
-  </style>
+  <link rel="stylesheet" href="../css/login_register_background.css"> 
+  <script src="../js/login_register_background.js" defer></script>
 </head>
 <body>
-  <div class="container mt-5">
-    <h2 class="text-center mb-4">Login</h2>
+<div class="bg-image left"></div>
+<div class="bg-image right"></div>
     <div class="login-container">
-    <form action="../action/login_user_action.php" method="POST">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" class="form-control" id="username" name="username" >
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-    <p class="mt-3">Don't have an account? <a href="./register_view.php">Register here</a></p>
+      <div class="container mt-5">
+        <h2 style="color: #1981b9"class="text-center mb-4"> Welcome To Your Campus Event Schedular</h2>
+        <h2 class="text-center mb-4">Login Here!</h2>
+      <form action="../action/login_user_action.php" method="POST">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" name="username" >
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+      <p class="mt-3">Don't have an account? <a  style="color: #1981b9" href="register_view.html">Register here</a></p>
     </div> 
-    <?php
+<?php
     check_login_errors(); 
     ?>
-  </div>
-
+  </div> 
 </body>
 </html>
